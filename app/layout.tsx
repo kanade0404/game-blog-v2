3a2b0e8b94cc7140814d62e75528fcfdfb495793
kanade0404 <melty0404@gmail.com>
2023-07-16 20:05:17 +0900

    :+1: add Google Adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from "./components/Analytics";
 import Main from "./components/Main";
 
 import "./global.scss";
+import Head from "next/head";
 const siteName = "kanade.log";
 const description = "主にゲームや麻雀についての感想・考察を書いています。";
 const url = "https://kanade0404.com";
@@ -43,6 +44,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3827420749431145"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <Analytics />
       <body>
         <Main>{children}</Main>
