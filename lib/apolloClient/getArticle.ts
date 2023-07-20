0c78245a9705bc90efb789b45162be442a2ec1b6
kanade0404 <melty0404@gmail.com>
2023-07-20 23:41:40 +0900

    :bug: queryごとにno-cache設定

--- lib/apolloClient/getArticle.ts
+++ lib/apolloClient/getArticle.ts
@@ -3,5 +3,9 @@ import { BlogDocument, BlogQuery } from "../api/query";
 import { cache } from "react";
 
 export const getArticle = cache(async (id: string) =>
-  getClient().query<BlogQuery>({ query: BlogDocument, variables: { id: id } }),
+  getClient().query<BlogQuery>({
+    query: BlogDocument,
+    variables: { id: id },
+    fetchPolicy: "no-cache",
+  }),
 );
--- lib/apolloClient/getArticleList.ts
+++ lib/apolloClient/getArticleList.ts
@@ -3,5 +3,5 @@ import { BlogListDocument, BlogListQuery } from "../api/query";
 import { cache } from "react";
 
 export const getArticleList = cache(async () =>
-  getClient().query<BlogListQuery>({ query: BlogListDocument }),
+  getClient().query<BlogListQuery>({ query: BlogListDocument, fetchPolicy: "no-cache" }),
 );
