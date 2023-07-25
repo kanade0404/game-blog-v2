8a98ae3a1ddf960419ecb2e0974cfdbdc1544864
kanade0404 <melty0404@gmail.com>
2023-07-25 22:50:08 +0900

    :+1: fix

--- lib/time/convertToyyyyMMdd.ts
+++ lib/time/convertToyyyyMMdd.ts
@@ -1,6 +1,7 @@
 import { format, parseISO } from "date-fns";
+import utcToZonedTime from "date-fns-tz/utcToZonedTime";
 
 type DateString = string;
 type ConvertToYYYYMMdd = (date: DateString) => DateString;
 export const convertToYYYYMMdd: ConvertToYYYYMMdd = (date) =>
-  format(parseISO(date), "yyyy. MM. dd");
+  format(utcToZonedTime(parseISO(date), "Asia/Tokyo"), "yyyy. MM. dd");
