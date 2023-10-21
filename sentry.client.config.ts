a162b5579374ae5ff1f12cba734cef0df26f0e74
kanade0404 <melty0404@gmail.com>
2023-10-21 16:52:17 +0900

    :+1: add client config and fix server config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,12 +2,9 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://87b41b01b0c4c3f2f1e1477b7ad6542f@o4505703537115136.ingest.sentry.io/4506086713720832",
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.5,
-
+  // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
