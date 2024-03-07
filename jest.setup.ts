a45b487095f5099f7f28d2c0ac533fe951f766a6
kanade0404 <melty0404@gmail.com>
2024-03-08 00:13:58 +0900

    :sparkles: jestからvitestへ移行 huskyとか削除してpnpm i --productionでビルドできるようにする

--- jest.setup.ts
+++
@@ -1,6 +0,0 @@
-import "@testing-library/jest-dom";
-
-// routerのモック
-// https://github.com/scottrippey/next-router-mock#dynamic-routes
-jest.mock("next/router", () => require("next-router-mock"));
-jest.mock("next/dist/client/router", () => require("next-router-mock"));
--- lib/time/convertToyyyyMMdd.spec.ts
+++ lib/time/convertToyyyyMMdd.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, test } from "vitest";
 import { convertToYYYYMMdd } from "./convertToyyyyMMdd";
 describe("convertToyyyyMMdd", () => {
 	test.each([
