9322b7bf03d943b33b1059f4790782e5f4d535a2
kanade0404 <melty0404@gmail.com>
2024-04-01 09:05:08 +0900

    :tada: Draft Modeを導入する DatoCMS自体にはpreview modeがないのでdraft mode経由でpreviewをみる

---
+++ app/api/draft/route.ts
@@ -0,0 +1,19 @@
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function GET(req: Request) {
+	const { searchParams } = new URL(req.url);
+	const secret = searchParams.get("secret");
+	const recordID = searchParams.get("id");
+	if (
+		!process.env.DRAFT_MODE_TOKEN ||
+		secret !== (process.env.DRAFT_MODE_TOKEN as string)
+	) {
+		return new Response("Invalid secret", { status: 401 });
+	}
+	if (!recordID) {
+		return new Response("No slug provided", { status: 400 });
+	}
+	draftMode().enable();
+	return redirect(`/article/${recordID}`);
+}
